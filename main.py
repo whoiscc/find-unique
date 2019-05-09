372fc6531016a2c0028e7924e3327a3f0defe42b
whoiscc <correctizer@gmail.com>
2019-05-09 03:23:51 -0700

    Add Travis CI config
    Signed-off-by: whoiscc <correctizer@gmail.com>

--- main.py
+++ main.py
@@ -13,67 +13,67 @@ def remain_memory(total):
 class Recorder:
     def __init__(self):
         self.count = 0
-        # unique_map[<word>] = <sequencial number>
+        # unique_map[<word>] = <sequencial number> || -1 if duplicated
         self.unique_map = {}
-        self.duplicated = set()
 
     def update_map(self, word):
-        # print('word: ', word)
-        # alternative way is store +Inf for duplicated words
-        # benchmark required here
-        if word in self.duplicated:
-            return
         if word not in self.unique_map:
             self.unique_map[word] = self.count
             self.count += 1
         else:
-            del self.unique_map[word]
-            self.duplicated.add(word)
+            self.unique_map[word] = -1
 
-recorder = Recorder()
-total_len = 0
-half_word = ''
-total_memory = (
-    DEFAULT_MAX_MEMORY_SIZE if len(sys.argv) <= 1
-    else int(sys.argv[1]) * 2 ** 20  # command line argument in MB
-)
-while True:
-    # in the worst case, every read word is unique, and all the input except
-    # whitespaces must be copied into `recorder`
-    # so we must make sure usable memory is at least double than input batch
-    # size
-    # the factor could change in practice according to input's feature
-    read_size = remain_memory(total_memory) // 2
-    # read_size = min(read_size, 4 * 2 ** 30)
-    # print('read size: ', read_size)
-    if read_size <= 0:
-        print('No enough memory.', file=sys.stderr)
-        sys.exit()
-    input_string = sys.stdin.read(read_size)
-    if input_string == '':
-        break
 
-    input_len = len(input_string)
-    # cursor may be -1 if ' ' is not found, which is expected
-    cursor = input_string.find(' ')
-    word = half_word + input_string[:cursor]
-    # print('joined word: ', word)
-    recorder.update_map(word)
+def main():
+    recorder = Recorder()
+    total_len = 0
+    half_word = ''
+    total_memory = (
+        DEFAULT_MAX_MEMORY_SIZE if len(sys.argv) <= 1
+        else int(sys.argv[1]) * 2 ** 20  # command line argument in MB
+    )
     while True:
-        while cursor < input_len and input_string[cursor] == ' ':
-            cursor += 1
-        next_cursor = input_string.find(' ', cursor)
-        if next_cursor < 0:
-            half_word = input_string[cursor:]
+        # in the worst case, every read word is unique, and all the input except
+        # whitespaces must be copied into `recorder`
+        # so we must make sure usable memory is at least double than input batch
+        # size
+        # the factor could change in practice according to input's feature
+        read_size = remain_memory(total_memory) // 2
+        # read_size = min(read_size, 4 * 2 ** 30)
+        # print('read size: ', read_size)
+        if read_size <= 0:
+            print('No enough memory.', file=sys.stderr)
+            sys.exit()
+        input_string = sys.stdin.read(read_size)
+        if input_string == '':
             break
-        word = input_string[cursor:next_cursor]
+
+        input_len = len(input_string)
+        # cursor may be -1 if ' ' is not found, which is expected
+        cursor = input_string.find(' ')
+        word = half_word + input_string[:cursor]
+        # print('joined word: ', word)
         recorder.update_map(word)
-        cursor = next_cursor
+        while True:
+            while cursor < input_len and input_string[cursor] == ' ':
+                cursor += 1
+            next_cursor = input_string.find(' ', cursor)
+            if next_cursor < 0:
+                half_word = input_string[cursor:]
+                break
+            word = input_string[cursor:next_cursor]
+            recorder.update_map(word)
+            cursor = next_cursor
 
-if half_word != '':
-    recorder.update_map(half_word)
+    if half_word != '':
+        recorder.update_map(half_word)
 
-# print(unique_map)
-# input data makes sure that there's at least one unique word
-# so there will always be at least one tuple in `unique_map.items()`
-print(min(recorder.unique_map.items(), key=lambda p: p[1])[0])
+    # print(unique_map)
+    # input data makes sure that there's at least one unique word
+    # so there will always be at least one tuple in `unique_map.items()`
+    print(min(
+        [pair for pair in recorder.unique_map.items() if pair[1] >= 0],
+        key=lambda p: p[1]
+    )[0])
+
+main()
--- test.py
+++ test.py
@@ -4,18 +4,20 @@ from sys import argv, exit as sys_exit
 from subprocess import run
 
 input_size = int(argv[1]) * 2 ** 20
-memory_limit = int(argv[2]) * 2 ** 20
+memory_limit = int(argv[2])
 repeat = int(argv[3])
 
 for i in range(repeat):
     print(f'running #{i + 1:04} ', end='', flush=True)
     p = run(
-        f'./generate.py {input_size} | python3 main.py {memory_limit}',
+        f'./generate.py {input_size} | mprof run python3 main.py {memory_limit * 2 ** 20}',
         capture_output=True, text=True, shell=True
     )
     expected = p.stderr
     actual = p.stdout
-    if expected.startswith('<Nothing>') or actual == expected:
+    if expected.startswith('<Nothing>'):
+        print('passed (nothing)')
+    elif any([expected.strip() == line for line in actual.split('\n')]):  # there are lines produced by mprof
         print('passed')
     else:
         print('failed')
@@ -23,4 +25,17 @@ for i in range(repeat):
         print(expected)
         print('actual:')
         print(actual)
+        sys_exit(1)
+
+    p = run('cat mprofile_*', capture_output=True, text=True, shell=True)
+    max_memory_usage = 0
+    for line in p.stdout.split('\n'):
+        # print(line)
+        if not line.startswith('MEM'):
+            continue
+        memory_usage = float(line.split(' ')[1])
+        max_memory_usage = max(max_memory_usage, memory_usage)
+    run('mprof clean', shell=True)
+    if max_memory_usage > memory_limit:
+        print(f'out of memory (max: {max_memory_usage} limit: {memory_limit})')
         sys_exit(1)
