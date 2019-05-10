fb53bd7d8bbaf08f65d3a4a9d1218a62759ce3db
whoiscc <correctizer@gmail.com>
2019-05-09 17:29:46 -0700

    Update test and Travis CI config
    Signed-off-by: whoiscc <correctizer@gmail.com>

--- test.py
+++ test.py
@@ -3,21 +3,24 @@
 from sys import argv, exit as sys_exit
 from subprocess import run
 
-input_size = int(argv[1]) * 2 ** 20
+input_size = int(argv[1])
 memory_limit = int(argv[2])
-repeat = int(argv[3])
+sample_count = int(argv[3])
+repeat = int(argv[4])
 
 for i in range(repeat):
     print(f'running #{i + 1:04} ', end='', flush=True)
     p = run(
-        f'./generate.py {input_size} | mprof run python3 main.py {memory_limit * 2 ** 20}',
+        f'./generate.py {input_size} {sample_count} | ./main {memory_limit}',
         capture_output=True, text=True, shell=True
     )
     expected = p.stderr
     actual = p.stdout
-    if expected.startswith('<Nothing>'):
-        print('passed (nothing)')
-    elif any([expected.strip() == line for line in actual.split('\n')]):  # there are lines produced by mprof
+    if 'out of memory' in expected:
+        print('out of memory')
+    elif expected.startswith('<nothing>'):
+        print('nothing')
+    elif expected == actual:
         print('passed')
     else:
         print('failed')
@@ -25,17 +28,4 @@ for i in range(repeat):
         print(expected)
         print('actual:')
         print(actual)
-        sys_exit(1)
-
-    p = run('cat mprofile_*', capture_output=True, text=True, shell=True)
-    max_memory_usage = 0
-    for line in p.stdout.split('\n'):
-        # print(line)
-        if not line.startswith('MEM'):
-            continue
-        memory_usage = float(line.split(' ')[1])
-        max_memory_usage = max(max_memory_usage, memory_usage)
-    run('mprof clean', shell=True)
-    if max_memory_usage > memory_limit:
-        print(f'out of memory (max: {max_memory_usage} limit: {memory_limit})')
         sys_exit(1)
