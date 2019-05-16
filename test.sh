591defd50f4e80e5c589a0c8874d284fdd5e88fd
whoiscc <correctizer@gmail.com>
2019-05-16 10:07:57 -0700

    Fix test script
    Signed-off-by: whoiscc <correctizer@gmail.com>

--- test.sh
+++ test.sh
@@ -2,8 +2,16 @@
 
 set -e
 
+change_word_len() {
+  if [[ "$OSTYPE" == "darwin"* ]]; then
+    sed -i '' "s/\(AVER_WORD_LEN =\) .*/\1 $1/" config.py
+  else
+    sed -i "s/\(AVER_WORD_LEN =\) .*/\1 $1/" config.py
+  fi
+}
+
 # short word
-sed -i 's/\(AVER_WORD_LEN =\) [0-9]*/\1 10/' config.py
+change_word_len 10
 for i in {1..10}
 do
   python gen.py 2> expect.txt | python main.py > real.txt
@@ -11,7 +19,7 @@ do
 done
 
 # long word
-sed -i 's/\(AVER_WORD_LEN =\) [0-9]*/\1 1 << 9/' config.py
+change_word_len '1 << 9'
 for i in {1..10}
 do
   python gen.py 2> expect.txt | python main.py > real.txt
